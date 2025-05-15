c676991839791e0a4070ba2fcb7e16ad97dda595
Guilherme Pacianotto <gpacianotto@gmail.com>
2025-05-15 14:53:50 -0300

    commit inicial + tela de login + tela de onboarding funcionando

---
+++ src/app/page.tsx
@@ -0,0 +1,211 @@
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Button, Input, InputLabel } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
+import ModalError from "@/components/ModalError";
+import ModalSuccess from "@/components/ModalSuccess";
+
+export default function Home() {
+
+  const [isRegister, setIsRegister] = useState(false);
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [registerName, setRegisterName] = useState("");
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [registerConfirmPassword, setRegisterConfirmPassword] = useState("");
+
+  return (
+    <>
+    <div className={styles.auxiliarContainer}>
+      <div></div>
+      <div className={styles.container}>
+        <h1>Bem vindo ao NeoSkoob!</h1>
+        <p>O seu lugar para criar suas resenhas dos seus filmes favoritos!</p>
+      <div>
+        { !isRegister &&
+          <form className={styles.loginForm}>
+            <h2>Login</h2>
+            <div>
+              <InputLabel htmlFor="email">Email:</InputLabel>
+              <Input 
+                type="email" 
+                id="email" 
+                name="email" 
+                required 
+                value={loginEmail}
+                onChange={(e) => setLoginEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <InputLabel htmlFor="password">Senha:</InputLabel>
+              <Input 
+                type="password" 
+                id="password" 
+                name="password" 
+                required
+                value={loginPassword}
+                onChange={(e) => setLoginPassword(e.target.value)} 
+              />
+            </div>
+            <Button 
+              onClick={(e) => {
+                axios.post(
+                  `http://localhost:3001/auth/login`, 
+                  {
+                    email: loginEmail,
+                    password: loginPassword
+                  }
+                  
+                ).then((res) => {
+                  console.log("Login result", res);
+
+                  
+
+                })
+                .catch((err) => {
+                  console.log("Login error", err.response.data.message);
+                  
+                  if(err.response.data.message) {
+                    setErrorMessage(err.response.data.message);
+                    setError(true);
+                    return;
+                  }
+                  
+                  setErrorMessage("Erro ao fazer login");
+                  setError(true);
+                });
+
+              }}
+            > 
+              Login
+            </Button>
+            <Button 
+              color="secondary"
+              onClick={() => setIsRegister(!isRegister)}
+            >
+              Registre-se
+            </Button>
+          </form>
+        }
+
+        { isRegister &&
+          <form className={styles.registerForm}>
+            <h2>Registre-se</h2>
+            <div>
+                <InputLabel htmlFor="name">Seu Nome:</InputLabel>
+                <Input
+                  type="text"
+                  id="name"
+                  name="name"
+                  required
+                  value={registerName}
+                  onChange={(e) => setRegisterName(e.target.value)}
+                />
+              
+            </div>
+            <div>
+              <InputLabel htmlFor="email">Email:</InputLabel>
+              <Input 
+                type="email" 
+                id="email" 
+                name="email" 
+                required
+                value={registerEmail}
+                onChange={(e) => setRegisterEmail(e.target.value)} 
+              />
+            </div>
+            <div>
+              <InputLabel htmlFor="password">Senha:</InputLabel>
+              <Input
+                type="password"
+                id="password"
+                name="password"
+                required
+                value={registerPassword}
+                onChange={(e) => setRegisterPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <InputLabel htmlFor="confirmPassword">Confirme sua Senha:</InputLabel>
+              <Input
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                required
+                value={registerConfirmPassword}
+                onChange={(e) => setRegisterConfirmPassword(e.target.value)}
+              />
+            </div>
+            <Button 
+              onClick={(e) => {
+                if(registerPassword !== registerConfirmPassword) {
+                  setErrorMessage("As senhas não conferem");
+                  setError(true);
+                  return;
+                }
+
+                axios.post(
+                  `http://localhost:3001/onboarding`, 
+                  {
+                    name: registerName,
+                    email: registerEmail,
+                    password: registerPassword
+                  }
+                ).then((res) => {
+                  console.log("Register result", res);
+
+                  if(res?.data) {
+                    setSuccessMessage("Conta regristrada com sucesso!");
+                    setSuccess(true);
+                    return;
+                  }
+                })
+                .catch((err) => {
+                  console.log("Register error", err.response.data.message);
+                  
+                  if(err.response.data.message) {
+                    setErrorMessage(err.response.data.message);
+                    setError(true);
+                    return;
+                  }
+                  
+                  setErrorMessage("Erro ao fazer registro");
+                  setError(true);
+                })
+
+              }}
+            >
+              Register
+            </Button>
+            <Button 
+              color="secondary"
+              onClick={() => setIsRegister(!isRegister)}
+            >
+              Volte ao Login
+            </Button>
+          </form>
+        }
+
+      </div>
+      </div>
+      <div>
+        <ModalError open={error} handleClose={() => {setError(false)}} message={errorMessage}/>
+        <ModalSuccess 
+          open={success} 
+          handleClose={() => {setSuccess(false); setIsRegister(false);}} 
+          message={successMessage}
+
+        />
+        </div>
+    </div>
+    </>
+  );
+}
---
+++ src/components/ModalError.tsx
@@ -0,0 +1,33 @@
+import { Box, Modal, Typography } from "@mui/material";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ModalError({ open, handleClose, message }: { open: boolean, handleClose: () => void, message: string }) {
+  return <>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        <Typography color="error" id="modal-modal-title" variant="h6" component="h2">
+          Erro!
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          {message}
+        </Typography>
+      </Box>
+    </Modal>
+  </>
+}
